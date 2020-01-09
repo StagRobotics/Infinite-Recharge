01d53a900e2f3fed785cfcf4556879bd0799b49c
joeNordling <joenordling@icloud.com>
2020-01-08 20:59:23 -0600

    Added the ColorSensoring Library + Stage2 Color Wheel Complete
    Completed the function to do the Stage 2 color wheel automatically.
    
    This allows drivers to drive up and hit a button which will then complete the color wheel
    
    Still need to add Timer inbetween steps

--- src/main/java/frc/robot/subsystems/RecordPlayer.java
+++ src/main/java/frc/robot/subsystems/RecordPlayer.java
@@ -23,10 +23,13 @@ import frc.robot.RobotMap;
 // 2. Add Colors to the ColorMatch
 // 3. Get the Reading from the Sensor and find the Closest Color from the ColorMatch
 // 4. Return the String
+// 5. Do stuff with the String
 
 public class RecordPlayer extends Subsystem {
   
   private final double ARMSPEED = 0.5;
+  private final boolean ISINVERSEROTOR = false;
+  private final int NUMBEROFTIMESAFTER = 2;
 
   // Step 1
   private final ColorMatch m_colorMatcher = new ColorMatch();
@@ -69,16 +72,92 @@ public class RecordPlayer extends Subsystem {
     ToneArm.set(0.0);
   }
 
+  public void turnWheel(boolean IsInverseRotor){
+    if(IsInverseRotor){
+      Rotor.set(Relay.Value.kForward);
+    } else {
+      Rotor.set(Relay.Value.kReverse);
+    }
+  }
+
+  public void stopWheel(){
+    Rotor.set(Relay.Value.kOff);
+  }
+
   public String getColorString(){
-    String result = "";
+
+    String colorString;
+
     Color detectedColor = Stylus.getColor();
+
     // Step 3
     ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
 
-    return result;
+    // Step 4
+    return colorString;
   }
   public void completeStage2(){
-    moveArmOut();
-  }
+    int count = 0;
 
+    // Moves toneArmOut
+    moveArmOut();
+
+    // Get whatever color is under the sensor when the needle is first dropped
+    String startingColor = getColorString();
+    String lastColor = getColorString();
+
+    // Turns the wheel in a given direction
+    turnWheel(ISINVERSEROTOR);
+
+    // Each color is on the wheel twice and therefore we need to rotate at least three times
+    // Each "count" is equal to half of a rotation so 3 * 2 = 6
+    // Therefore continue using the motor until we see the color 6 times
+    while(count < 6){
+      // Get whatever the motor currently sees
+      String currentColor = getColorString();
+
+      // If the currentColor is equal to staring color, then we know we have completed a 1/2 rotation so increase the count by 1
+      // Last color prevents the sensor from reading the same panel twice
+      if(currentColor.equals(startingColor) && !currentColor.equals(lastColor)){
+        count++;
+      }
+
+      // Sets the lastColor read to the currentColor
+      lastColor = currentColor;
+    }
+
+    // We want to spin the color wheel a little over three times to make sure the sensor on the field sees three complete rotations
+    // Used to count the Colors past the final starting color
+    int nextColorCounter = 0;
+
+    // NUMBEROFTIMESAFTER is the number of colors past the final third rotation
+    while(nextColorCounter < NUMBEROFTIMESAFTER){
+      String currentColor = getColorString();
+
+      // If the current color is different than the previous color
+      if(!currentColor.equals(lastColor)){
+        nextColorCounter++;
+        
+        // Set the last Color read to the starting color
+        lastColor = currentColor;
+      }
+    }
+
+    // Stops Wheel
+    stopWheel();
+
+    // Raises arm back into chassis
+    moveArmIn();
+  }
 }
