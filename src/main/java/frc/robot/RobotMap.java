0d9b7ef4a1a41033fb020f905ae22ce2a51f306b
joeNordling <joenordling@icloud.com>
2020-02-05 20:25:49 -0600

    Added Encoders

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,4 +14,10 @@ public class RobotMap {
 
     public static Port StylusPort = Port.kOnboard;
 
+    public static int leftEncoderPort1 = 0;
+    public static int leftEncoderPort2 = 1;
+
+    public static int rightEncoderPort1 = 2;
+    public static int rightEncoderPort2 = 3;
+
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,7 +7,7 @@ import frc.robot.Robot;
 import frc.robot.RobotMap;
 
 import frc.robot.commands.*;
-
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -24,6 +24,8 @@ public class DriveTrain extends Subsystem {
 
 	private final double DEADBAND = 0.05;
 
+	private final double WHEEL_DIAMETER = 6.0;
+
 	// Initialize Motor Controllers
 
 	private PWMVictorSPX leftMotor = new PWMVictorSPX(RobotMap.leftMotor);
@@ -34,6 +36,9 @@ public class DriveTrain extends Subsystem {
 
 	public DifferentialDrive robotDrive = new DifferentialDrive(leftMotor, rightMotor);
 
+	public Encoder leftEncoder = new Encoder(RobotMap.leftEncoderPort1, RobotMap.leftEncoderPort2, false, Encoder.EncodingType.k4X);
+	public Encoder rightEncoder = new Encoder(RobotMap.rightEncoderPort1, RobotMap.rightEncoderPort2, false, Encoder.EncodingType.k4X);
+
 	// Creates the DriveTrain Subsystem
 
 	public DriveTrain() {
@@ -42,6 +47,22 @@ public class DriveTrain extends Subsystem {
 
 		System.out.println("Drive");
 
+		leftEncoder.setMaxPeriod(1);
+		leftEncoder.setMinRate(10);
+		leftEncoder.setDistancePerPulse(0.00275);
+		leftEncoder.setReverseDirection(true);
+		leftEncoder.setSamplesToAverage(7);
+		leftEncoder.reset();
+
+		rightEncoder.setMaxPeriod(1);
+		rightEncoder.setMinRate(10);
+		rightEncoder.setDistancePerPulse(0.00275);
+		rightEncoder.setReverseDirection(true);
+		rightEncoder.setSamplesToAverage(7);
+		rightEncoder.reset();
+
+		addChild("Left Encoder", leftEncoder);
+		addChild("Right Encoder", rightEncoder);
 	}
 
 	@Override
@@ -58,6 +79,17 @@ public class DriveTrain extends Subsystem {
 		SmartDashboard.putNumber("Right Motor", rightMotor.get());
 
 		SmartDashboard.putString("Color Reading", Robot.m_RecordPlayer.getColorString());
+
+		SmartDashboard.putNumber("Encoder Left", getLeftInches());
+		SmartDashboard.putNumber("Encoder Right", getRightInches());
+	}
+
+	public double getLeftInches() {
+		return leftEncoder.getDistance() * Math.PI * WHEEL_DIAMETER;
+	}
+
+	public double getRightInches() {
+		return rightEncoder.getDistance() * Math.PI * WHEEL_DIAMETER;
 	}
 
 	public void drive(double leftY, double rightY) {
